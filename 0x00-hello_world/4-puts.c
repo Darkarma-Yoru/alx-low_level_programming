ab2c5357f7bf3496f22d145db993bca6b1f54d9e
Darren Allworth <darrenmallworth@gmail.com>
2023-02-16 16:39:11 -0800

    task 3.1 and 4.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/*
+*main = entry point
+*description: This C Program print with a put functiom
+*return: always 0 (success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
