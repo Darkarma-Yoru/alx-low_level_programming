225f736014be8ffc64afeb135273329a16b295e2
Darkarma-Yoru <darrenmallworth@gmail.com>
2023-02-21 15:48:17 +0200

    task 0.1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(){
-
+int main(void){
+/*
+* main - Entry Point
+* Description: prints '_putchar\n'
+* Return: Always 0 (success)
+*/
     printf("_putchar\n");
 
     return 0;
