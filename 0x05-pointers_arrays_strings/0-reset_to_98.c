4f381de92d50f95dcbdf54e52bf4673e42923f9d
Darkarma-Yoru <darrenmallworth@gmail.com>
2023-02-27 13:16:37 +0200

    task 0.4

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 *  reset_to_98 - takes the pointer and updates the value
 *  @n: int to check
-*  Return: 0 is success
+*  Return: nothing
 */
-    void reset_to_98(int *n){
 
+void reset_to_98(int *n)
+{
         *n =98;
 }
